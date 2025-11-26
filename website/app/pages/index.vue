df85f61c491e0f38a37452f800339caa13b83f2f
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-11-26 22:45:53 +0100

    initial setup

---
+++ website/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aliquid quis
+    exercitationem perferendis dolorem. Magnam reprehenderit at et labore
+    asperiores ipsum iure, quidem quas iusto, modi ad atque magni consequatur?
+  </div>
+</template>
---
+++ website/content/announcing-backoffice.md
@@ -0,0 +1,1 @@
+# Introducing BAckoffice.vue
