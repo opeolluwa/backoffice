9c57461ebe01c2f732f780f3afd754691946440c
opeolluwa <adefemiadeoye@yahoo.com>
2026-07-11 00:52:13 +0100

    chore

---
+++ backoffice_console_fe/app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto px-4 py-5">
+    <AppLeadingText class="text-left">Backoffice Console</AppLeadingText>
+    <p>Generated and manage app components</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- backoffice_console_fe/app/pages/index.vue
+++ backoffice_console_fe/app/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-hey
+<div class="flex flex-wrap gap-4">
+
+    <div class="" v-for="component in conponents" :key="component.name"></div>
+</div>
 </template>
+
+
+<script lang="ts" setup>
+const conponents = ref([
+  {
+    name: "entity",
+    description: "create entities",
+    icon: ""
+  }
+])
+</script>
